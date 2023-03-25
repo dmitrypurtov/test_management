d46e28c1f4ca9ce3baf2699672fba147976334e5
dmitry.purtov <dmitry.purtov@outlook.com>
2023-03-25 18:08:32 +0300

    feat: Структура проекта

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
+import styles from './App.module.css'; 
 import './App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className={styles.error}>
           Edit <code>src/App.tsx</code> and save to reload. 1
         </p>
         <a
